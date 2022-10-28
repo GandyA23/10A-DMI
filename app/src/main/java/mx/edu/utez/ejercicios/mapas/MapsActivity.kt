a23ecc97986a46c73d6425c9fb6ae38c4835f4c0
Gandy Esaú Ávila Estrada <20193tn128@utez.edu.mx>
2022-10-28 18:47:05 -0500

    Se agregan funciones de Localización del centro de la pantalla

--- app/src/main/java/mx/edu/utez/ejercicios/mapas/MapsActivity.kt
+++ app/src/main/java/mx/edu/utez/ejercicios/mapas/MapsActivity.kt
@@ -1,7 +1,12 @@
 package mx.edu.utez.ejercicios.mapas
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -11,6 +16,8 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import mx.edu.utez.ejercicios.R
 import mx.edu.utez.ejercicios.databinding.ActivityMapsBinding
+import java.lang.Exception
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -41,9 +48,88 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Add a marker in UTEZ and move the camera
+        val utez = LatLng(18.850253, -99.200730)
+        mMap.addMarker(MarkerOptions().position(utez).title("UTEZ"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(utez))
+
+        // Realiza el chequeo de los permisos
+        // En caso
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            return
+        }
+
+        mMap.isMyLocationEnabled = true
+        getMarkerLocation()
+    }
+
+    /**
+     * Obtiene la latitud y la longitud en base a la posición central de la camara
+     * */
+    fun getMarkerLocation() {
+        mMap.setOnCameraIdleListener {
+            var lat = mMap.cameraPosition.target.latitude
+            var lon = mMap.cameraPosition.target.longitude
+
+            Log.d("MapsLog", "$lat $lon")
+            getDirection(lat, lon)
+        }
+    }
+
+    /**
+     * Obtiene un string de dirección dado una latitude y longitude*/
+    fun getDirection(lat: Double, lon: Double) {
+        try {
+            var geocoder = Geocoder(this, Locale.getDefault())
+            var direcciones = geocoder.getFromLocation(lat, lon, 1)
+
+            // Dirección completa en un input
+            var direccionCompleta = direcciones[0].getAddressLine(0)
+            Log.d("MapsLog", "Tu dirección es $direccionCompleta")
+
+            if (direcciones.size > 0) {
+                var pais = direcciones[0].countryName
+                var estado = direcciones[0].adminArea
+                var municipio = direcciones[0].locality
+                var calle = direcciones[0].thoroughfare
+                var colonia = direcciones[0].subLocality
+                var codigoPostal = direcciones[0].postalCode
+
+                if (pais != null)
+                    Log.d("MapsLog", "Tu país es $pais")
+
+                if (estado != null)
+                    Log.d("MapsLog", "Tu estado es $estado")
+
+                if (municipio != null)
+                    Log.d("MapsLog", "Tu municipio es $municipio")
+
+                if (calle != null)
+                    Log.d("MapsLog", "Tu calle es $calle")
+
+                if (colonia != null)
+                    Log.d("MapsLog", "Tu colonia es $colonia")
+
+                if (codigoPostal != null)
+                    Log.d("MapsLog", "Tu Código Postal es $codigoPostal")
+            }
+        } catch (e: Exception) {
+            Log.e("MapsLog", e.message!!)
+        }
     }
 }
--- app/src/main/java/mx/edu/utez/ejercicios/romano/RomanoActivity.kt
+++ app/src/main/java/mx/edu/utez/ejercicios/romano/RomanoActivity.kt
@@ -10,15 +10,10 @@ class RomanoActivity : AppCompatActivity() {
     lateinit var binding: ActivityRomanoBinding
 
     private val NUMS_BASE: Map<Int, String> = mapOf(
-        100 to "C",
-        90 to "XC",
-        50 to "L",
-        40 to "XL",
-        10 to "X",
-        9 to "IX",
-        5 to "V",
-        4 to "IV",
-        1 to "I",
+        1000 to "M",
+        900 to "CM", 500 to "D", 400 to "CD", 100 to "C",
+        90 to "XC", 50 to "L", 40 to "XL", 10 to "X",
+        9 to "IX", 5 to "V", 4 to "IV", 1 to "I"
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
